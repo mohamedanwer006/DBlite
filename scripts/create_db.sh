d183487f9f3ef79ce82656592551fbaaa6b8b0ac
mohamedanwer006 <mohamedaner006@gmail.com>
2022-05-20 16:07:25 +0200

    Add create database

---
+++ scripts/create_db.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#******************************************************************************
+#   Created at : 19/5/2022
+#******************************************************************************
+
+#   Create Database
+#   How to use
+#!  `. create_db.sh  <dbName>`
+#!  return exit code 1 database already exists
+#!  return exit code 0 database created 
+
+#   variables
+dbDir="DATABASES"
+dbName=$1
+
+
+
+# functions
+
+function createDb(){
+#   Check if database already exists
+    if [ -d "$dbDir/$1" ]
+    then
+        return 1
+    else 
+        mkdir "./$dbDir/$1"
+        return 0
+    fi
+   
+} 
+
+
+#   TODO: validate the name of the database
+
+#   Check if databases directory already exists
+
+if [ -d "$dbDir" ]
+then
+    
+    createDb $dbName
+    if [[ $? == 1 ]]
+    then
+    return 1
+    else
+    return 0
+    fi
+else 
+    mkdir $dbDir
+      createDb $dbName
+    if [[ $? == 1 ]]
+    then
+    return 1
+    else
+    return 0
+    fi
+fi
+
+
