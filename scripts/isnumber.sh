90e59781e1223453ba52590add4d0e594bba59ab
mohamedanwer006 <mohamedanwer006@gmail.com>
2022-05-26 20:01:25 +0200

    Update docs of script isnumber.sh

--- scripts/isnumber.sh
+++ scripts/isnumber.sh
@@ -2,6 +2,8 @@
 #   check if the argument is a digit number
 #   return 0 if it is a digit number
 #   return 1 if it is not a digit number
+#   this script run as a  sourced script
+#   to use it as a function . isnumber.sh <arg 
 
 if [[ $1 = *([0-9]) ]]
 then
@@ -9,4 +11,6 @@ then
 else
     return 1
 fi
+
+
 
