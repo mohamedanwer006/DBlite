9711addc467090d2d674dbdf6499cf618222f73e
mahmoud-m-selim <mahmoudmselim1@gmail.com>
2022-05-30 20:39:44 +0200

    Modifications to SELECT script

--- scripts/select_table.sh
+++ scripts/select_table.sh
@@ -2,16 +2,15 @@
 
 
 PS3="Enter option number: "
-dbDir="DATABASES"
+dbDir=$1 #"DATABASES"
 
-dbName=$1
+dbName=$2
 
 function index_of(){
 
 	my_array=$2[@]
 	arr=("${!my_array}")
 	value=$1
-
 	for i in "${!arr[@]}"; do
 		if [[ "${arr[$i]}" = "${value}" ]]; 
 		then
@@ -32,7 +31,7 @@ function selectMainMenu(){
 		if [[ $t == "Back" ]]
 		then break
 		fi
-		whereMenu $1 $t
+		whereMenu $dbDir/$dbName $t
 		#selectTableMenu $1 $t
 	done
 	PS3="Enter the operation number: "
@@ -83,12 +82,12 @@ function columnMenu(){
 			select col in "${columns[@]}";
 			do
 				if [[ $col == "Back" ]]
-				then break
+				then return
 				fi
 				
 				echo "Enter the column numbers to appear in select: "
 				
-				selectColumn $1 $t $REPLY
+				selectColumn $1 $t $REPLY # or $col instead of $REPLY
 			done
 			fi
 		done
@@ -98,47 +97,98 @@ function columnMenu(){
 	fi
 	
 	
-	echo "Select certain columns or Select * (All)?"
+	# here if user will select where
 	declare -a selectOptions=("Choose column(s)" "* (All)"  "Cancel")
-	PS3="Enter option number: "
+	PS3="Select certain columns or Select * (All)?"
 	select so in "${selectOptions[@]}";
 	do
 		if [[ $so == "Cancel" ]]
-		then break
+		then return
 		fi
 		
 		if [[ $so == "Choose column(s)" ]]
 		then
-			IFS="," read -r -a columns < <(head -n 1 "$dbDir/$dbName/$t")
+			IFS="," read -r -a columns < <(head -n 1 $1/$t)
 			columns+=("Back")
 		select col in "${columns[@]}";
 		do
 			if [[ $col == "Back" ]]
-			then break
+			then return
 			fi
-			read -r -p "Enter the column number: " selection #columns to appear after select executes
 			
-			#index_of $col columns #get column index
+			read -r -p "Enter the column numbers to appear after select executes.\n Seperate column numbers by commas and no spaces.\n Do not put a comma before the first column number\n or after the last column number. Example --> 2,5,6 : " selection #columns to appear after select executes
 			
-			echo "Next, select where column."
+			PS3="Next, select the where column: "
 			IFS="," read -r -a cond_columns < <(head -n 1 $1/$t)
 			select cond_column in "${cond_columns[@]}"
 			do	
-			echo > /dev/null
+				typeset -i cond_column_index
+				typeset -i cond_column_number
+
+				index_of $cond_column cond_columns #get column index
+				cond_column_index=$? 
+				read -r -p "Enter search criteria/value for that column: " criteria
+				
+				# Now use table($1/$t), condition_column and its index to filter records, selection to cut fields/columns that were selected
+				cond_column_number=$cond_column_index+1		
+				record_nums=( $(tail -n +3 $1/$t | cut -d, -f$cond_column_number | grep -n $criteria | cut -f1 -d:) )
+				
+				touch $1/$t.tmp
+				cut -d, -f$selection $1/$t | sed -n "1p" > $1/$t.tmp
+
+				for record_num in "${record_nums[@]}"; 
+				do
+					tail -n +3 $1/$t | cut -d, -f$selection | sed -n "${record_num}p" >> $1/$t.tmp # Remember to cut with $selection
+				done
+				
+				cat $1/$t.tmp
+				rm $1/$t.tmp
+				return
 			done
-
-			read -r -p "Enter search criteria/value for that column: " criteria
-
-			grep $criteria $1/$t | cut -d"," -f$selection | cat
 		done
 		fi
 
 		if [[ $so == "* or All" ]]; then
-			awk -F"," 'NR!=2' $1/$t > $1/$t.tmp
-			cat $1/$t.tmp
+						IFS="," read -r -a columns < <(head -n 1 $1/$t)
+			columns+=("Back")
+		select col in "${columns[@]}";
+		do
+			if [[ $col == "Back" ]]
+			then return
+			fi
+			
+			read -r -p "Enter the column numbers to appear after select executes.\n Seperate column numbers by commas and no spaces.\n Do not put a comma before the first column number\n or after the last column number. Example --> 2,5,6 : " selection #columns to appear after select executes
+			
+			PS3="Next, select the where column: "
+			IFS="," read -r -a cond_columns < <(head -n 1 $1/$t)
+			select cond_column in "${cond_columns[@]}"
+			do	
+				typeset -i cond_column_index
+				typeset -i cond_column_number
+
+				index_of $cond_column cond_columns #get column index
+				cond_column_index=$? 
+				read -r -p "Enter search criteria/value for that column: " criteria
+				
+				# Now use table($1/$t), condition_column and its index to filter records, selection to cut fields/columns that were selected
+				cond_column_number=$cond_column_index+1		
+				record_nums=( $(tail -n +3 $1/$t | cut -d, -f$cond_column_number | grep -n $criteria | cut -f1 -d:) )
+				
+				touch $1/$t.tmp
+				sed -n "1p" $1/$t > $1/$t.tmp
+
+				for record_num in "${record_nums[@]}"; 
+				do
+					tail -n +3 $1/$t | sed -n "${record_num}p" >> $1/$t.tmp # Remember to cut with $selection
+				done
+				
+				cat $1/$t.tmp
+				rm $1/$t.tmp
+				return
+			done
+		done
 		fi
 	done
-	PS3="Enter option number: "
 }
 
 
@@ -156,7 +206,7 @@ function selectWhere(){
 	#echo "FILTERR ${filter}"
 	awk -F"," -v column="$col_no" -v condition="$cond_no" -v -v fltr="$filter" '{ if ( $condition=="$fltr" ) {  print $0 } else { next } }' $1/$t > $1/buffer
 	cat $1/buffer
-	echo "Update succesful!"
+	echo "Update successful!"
 }
 
 selectMainMenu $1
