aa56374055f871ec0add8b2f38e4704a571534d6
mohamedanwer006 <mohamedanwer006@gmail.com>
2022-06-01 09:08:00 +0200

    Add name checks when user create table

--- scripts/create_table.sh
+++ scripts/create_table.sh
@@ -2,15 +2,25 @@
 #-------------------------------------------------
 # Script to create the table in the database
 #* How to use:
-#* . create_table.sh <database_name> <table_name>  
+#* . create_table.sh <database_name> <table_name>
 #*   <database_name> is the name of the database
 #*   <table_name> is the name of the table
+#* return exit code 1 table already exists
+#* return exit code 0 table created
 #-------------------------------------------------
 
 dbDir="DATABASES"
 
 dbName=$1
 tableName=$2
+
+
+
+if [ -f "$PWD/$dbDir/$dbName/$tableName"  ]
+then
+    return 1 # return 1 table already exists
+fi
+
 
 function createTable(){
     touch "$PWD/$dbDir/$dbName/$1"
@@ -19,7 +29,8 @@ function createTable(){
 # start by creating table file
 
 createTable "$tableName"
-# TODO : check table name
+# check if table name exist
+
 # Add entity to table
 finish=0 # variable to check if the user wants to add more entities --> 0 = yes , 1 = no
 
@@ -30,68 +41,69 @@ typeset -i counter=1
 
 PS3="Select the command number : "
 
-while [[ $finish == 0 ]]
-do 
-
-select n in "Add entity" "Finish"
-do
-case $n in
-    "Add entity" )
-        while true
+    while [[ $finish == 0 ]]
+    do
+        
+        select n in "Add entity" "Finish"
         do
-            read -rp "Enter entity no $counter  : " entityName
-            # check if the entity name is valid 
-            if . isvalidname.sh "$entityName"
-            then
-                # check entity name is not entered twice
-                if . isinlist.sh "$entityName" "${entities[@]}"
-                then
-                    echo "Entity name already exists"
-                else
-                    entities+=("$entityName")
+            case $n in
+                "Add entity" )
+                    while true
+                    do
+                        read -rp "Enter entity no $counter  : " entityName
+                        # check if the entity name is valid
+                        if . isvalidname.sh "$entityName"
+                        then
+                            # check entity name is not entered twice
+                            if . isinlist.sh "$entityName" "${entities[@]}"
+                            then
+                                echo "Entity name already exists"
+                            else
+                                entities+=("$entityName")
+                                break
+                            fi
+                        else
+                            echo "Invalid name of entity start with letter and contain only letters ,numbers and _"
+                        fi
+                    done
                     break
-                fi
-            else
-                echo "Invalid name of entity start with letter and contain only letters ,numbers and _"
-            fi
+                ;;
+                "Finish" )
+                    finish=1
+                    break
+                ;;
+                *) echo "invalid option";;
+            esac
         done
-        break
-        ;;
-    "Finish" )
-        finish=1
-        break
-        ;;
-    *) echo "invalid option";;
-esac
-done
+        
+        if [[ $finish == 1 ]]
+        then
+            break
+        fi
+        
+        select t in "String" "Integer"
+        do
+            case $t in
+                "String" )
+                    entitiesType+=("String")
+                    break
+                ;;
+                "Integer" )
+                    entitiesType+=("Integer")
+                    break
+                ;;
+                * ) echo "invalid option";;
+            esac
+        done
+        counter=$counter+1
+    done
 
-if [[ $finish == 1 ]]
-then
-    break
-fi
-
-select t in "String" "Integer"
-do 
-case $t in
-    "String" )
-        entitiesType+=("String")
-        break
-        ;;
-    "Integer" )
-        entitiesType+=("Integer")
-        break
-        ;;
-    * ) echo "invalid option";;
-esac
-done
-counter=$counter+1
-done
-
-# Add metadata to table 
-# First line contains the columns names
-# Second line contains the types of the columns
-IFS=","; echo "${entities[*]}" >> "$PWD/$dbDir/$dbName/$tableName"
-IFS=","; echo "${entitiesType[*]}" >> "$PWD/$dbDir/$dbName/$tableName"
-sed -i 's/String/1/g' "$PWD/$dbDir/$dbName/$tableName"
-sed -i 's/Integer/0/g' "$PWD/$dbDir/$dbName/$tableName"
-PS3="Select the command number : " 
+    # Add metadata to table
+    # First line contains the columns names
+    # Second line contains the types of the columns
+    IFS=","; echo "${entities[*]}" >> "$PWD/$dbDir/$dbName/$tableName"
+    IFS=","; echo "${entitiesType[*]}" >> "$PWD/$dbDir/$dbName/$tableName"
+    sed -i 's/String/1/g' "$PWD/$dbDir/$dbName/$tableName" # replace String with 1
+    sed -i 's/Integer/0/g' "$PWD/$dbDir/$dbName/$tableName" # replace Integer with 0
+    PS3="Select the command number : "
+    return 0 # return 0 if table created successfully
